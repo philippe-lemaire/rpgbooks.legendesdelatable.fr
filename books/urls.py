7f837201648fffd214cf5e856e4ef2c12f79c74b
Philippe Lemaire <phil@plemaire.net>
2026-09-20 16:38:01 +0200

    new links for view filtered by booktype

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "books"
 
 urlpatterns = [
-    path("", views.BooksIndexView.as_view(), name="index"),
+    # path("", views.BooksIndexView.as_view(), name="index"),
+    path("", views.book_list_view, name="index"),
     path("book/<slug:slug>", views.book_detail_view, name="book_detail"),
     path("systems/", views.SystemsListView.as_view(), name="systems_list"),
     path("systems/<slug:slug>", views.books_by_system_view, name="books_by_system"),
--- books/views.py
+++ books/views.py
@@ -13,15 +13,29 @@ from .models import Book, System, Tag
 # Create your views here.
 
 
+def fix_choice_display(qs):
+    for book in qs:
+        book.display_book_type = book.get_book_type_display()
+
+
 class BooksIndexView(ListView):
     context_object_name = "books"
     model = Book
     paginate_by = 10
 
 
+def book_list_view(request):
+    books = Book.objects.all()
+    fix_choice_display(books)
+    template_name = "books/book_list.html"
+    context = {"books": books}
+    return render(request, template_name, context)
+
+
 def books_by_system_view(request, slug):
     system = System.objects.filter(slug=slug)[0]
     queryset = Book.objects.filter(system=system)
+    fix_choice_display(queryset)
     template_name = "books/book_list.html"
     context = {"books": queryset, "filtered_by": f"System: {system}"}
     return render(request, template_name, context)
@@ -29,6 +43,7 @@ def books_by_system_view(request, slug):
 
 def books_by_book_type_view(request, book_type):
     queryset = Book.objects.filter(book_type=book_type)
+    fix_choice_display(queryset)
     template_name = "books/book_list.html"
     context = {
         "books": queryset,
@@ -41,6 +56,7 @@ def books_by_tag_view(request, slug):
     tag = Tag.objects.filter(slug=slug)[0]
     slug = tag.slug
     queryset = Book.objects.filter(tags__slug=slug)
+    fix_choice_display(queryset)
     template_name = "books/book_list.html"
     context = {"books": queryset, "filtered_by": f"Tag: {tag}"}
     return render(request, template_name, context)
