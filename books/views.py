76358eae28da4c44db100959e11e7bbc7e92f6a3
Philippe Lemaire <phil@plemaire.net>
2026-09-22 18:35:20 +0200

    for loop for book genres and better handling of empty results

--- books/views.py
+++ books/views.py
@@ -19,51 +19,65 @@ def fix_choice_display(qs):
 
 
 def book_list_view(request):
-    books = Book.objects.all().order_by('system')
+    books = Book.objects.all().order_by("system")
     fix_choice_display(books)
     template_name = "books/book_list.html"
-    context = {"books": books}
+    context = {
+        "books": books,
+        "book_types": Book.BOOK_TYPE_CHOICES,
+    }
     return render(request, template_name, context)
 
 
 def books_by_want_to_run(request):
-    queryset = Book.objects.filter(want_to_run=True).order_by('system')
+    queryset = Book.objects.filter(want_to_run=True).order_by("system")
     fix_choice_display(queryset)
     template_name = "books/book_list.html"
     context = {
         "books": queryset,
         "filtered_by": "Want to run",
+        "book_type": Book.BOOK_TYPE_CHOICES,
     }
     return render(request, template_name, context)
 
 
 def books_by_system_view(request, slug):
     system = System.objects.filter(slug=slug)[0]
-    queryset = Book.objects.filter(system=system).order_by('system')
-    fix_choice_display(queryset)
-    template_name = "books/book_list.html"
-    context = {"books": queryset, "filtered_by": f"System: {system}"}
-    return render(request, template_name, context)
-
-
-def books_by_book_type_view(request, book_type):
-    queryset = Book.objects.filter(book_type=book_type).order_by('system')
+    queryset = Book.objects.filter(system=system).order_by("system")
     fix_choice_display(queryset)
     template_name = "books/book_list.html"
     context = {
         "books": queryset,
-        "filtered_by": f"Book type: {queryset[0].get_book_type_display()}",
+        "filtered_by": f"System: {system}",
+        "book_types": Book.BOOK_TYPE_CHOICES,
     }
+    return render(request, template_name, context)
+
+
+def books_by_book_type_view(request, book_type):
+    queryset = Book.objects.filter(book_type=book_type).order_by("system")
+    fix_choice_display(queryset)
+    template_name = "books/book_list.html"
+    context = {
+        "books": queryset,
+        "book_types": Book.BOOK_TYPE_CHOICES,
+    }
+    if queryset:
+        context["filtered_by"] = (f"Book type: {queryset[0].get_book_type_display()}",)
     return render(request, template_name, context)
 
 
 def books_by_tag_view(request, slug):
     tag = Tag.objects.filter(slug=slug)[0]
     slug = tag.slug
-    queryset = Book.objects.filter(tags__slug=slug).order_by('system')
+    queryset = Book.objects.filter(tags__slug=slug).order_by("system")
     fix_choice_display(queryset)
     template_name = "books/book_list.html"
-    context = {"books": queryset, "filtered_by": f"Tag: {tag}"}
+    context = {
+        "books": queryset,
+        "filtered_by": f"Tag: {tag}",
+        "book_types": Book.BOOK_TYPE_CHOICES,
+    }
     return render(request, template_name, context)
 
 
@@ -73,6 +87,7 @@ def book_detail_view(request, slug):
     template_name = "books/book_detail.html"
     context = {
         "book": book,
+        "book_types": Book.BOOK_TYPE_CHOICES,
     }
     return render(request, template_name, context)
 
